4357d944dc0291c095047ca8f2f0072af7fb5cc7
Mancopp <mancoppor@gmail.com>
2024-06-06 17:34:29 +0200

    Fix project sidebar links

--- src/Controller/ProjectController.php
+++ src/Controller/ProjectController.php
@@ -25,7 +25,37 @@ class ProjectController extends AbstractController
     }
 
     #[Route('/project/{id}/board', name: 'project_board')]
-    public function projects(Project $project): Response
+    public function project_board(Project $project): Response
+    {
+        $sidebar = $this->generateControllerSidebar($project);
+
+        return $this->render('project/board.html.twig', [
+            'sidebar' => $sidebar,
+        ]);
+    }
+
+    #[Route('/project/{id}/list', name: 'project_list')]
+    public function project_list(Project $project): Response
+    {
+        $sidebar = $this->generateControllerSidebar($project);
+
+        return $this->render('project/board.html.twig', [
+            'sidebar' => $sidebar,
+        ]);
+    }
+
+    #[Route('/project/{id}/settings', name: 'project_settings')]
+    public function project_settings(Project $project): Response
+    {
+        $sidebar = $this->generateControllerSidebar($project);
+
+        return $this->render('project/board.html.twig', [
+            'sidebar' => $sidebar,
+        ]);
+    }
+
+    #[Route('/project/{id}/participants', name: 'project_participants')]
+    public function project_participants(Project $project): Response
     {
         $sidebar = $this->generateControllerSidebar($project);
 
@@ -36,10 +66,10 @@ class ProjectController extends AbstractController
 
     private function generateControllerSidebar(Project $project) : array {
       $nav_buttons = [
-        ['text' => 'Board', 'route' => 'app_project', 'params' => ['id' => $project->getId()]], 
-        ['text' => 'List', 'route' => 'app_project', 'params' => ['id' => $project->getId()]],
-        ['text' => 'Project Settings', 'route' => 'app_project', 'params' => ['id' => $project->getId()]],
-        ['text' => 'Participants', 'route' => 'app_project', 'params' => ['id' => $project->getId()]],
+        ['text' => 'Board', 'route' => 'project_board', 'params' => ['id' => $project->getId()]], 
+        ['text' => 'List', 'route' => 'project_list', 'params' => ['id' => $project->getId()]],
+        ['text' => 'Project Settings', 'route' => 'project_settings', 'params' => ['id' => $project->getId()]],
+        ['text' => 'Participants', 'route' => 'project_participants', 'params' => ['id' => $project->getId()]],
         ['text' => 'Back to dashboard', 'route' => 'app_dashboard'],
       ];
 
